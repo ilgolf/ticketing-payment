55083cf4b0c3d5d035902e0d3ccfd6b30432bf62
Gyeongtae Noh <77387861+ilgolf@users.noreply.github.com>
2025-07-06 21:10:25 +0900

    fix. order service code Refactoring (#11)
    Co-authored-by: 노경태 <gyeongtae777@utransfer.com>

--- core/src/main/kotlin/me/golf/core/usecase/domain/order/usecase/message/CreateOrderResponseMessage.kt
+++ core/src/main/kotlin/me/golf/core/usecase/domain/order/usecase/message/CreateOrderResponseMessage.kt
@@ -1,8 +1,19 @@
 package me.golf.core.usecase.domain.order.usecase.message
 
+import me.golf.core.model.domain.order.Order
 import java.math.BigDecimal
 
 data class CreateOrderResponseMessage(
     val orderId: String,
     val amount: BigDecimal,
-)
+) {
+
+    companion object {
+        fun from(order: Order): CreateOrderResponseMessage {
+            return CreateOrderResponseMessage(
+                orderId = order.orderId,
+                amount = order.amount,
+            )
+        }
+    }
+}
