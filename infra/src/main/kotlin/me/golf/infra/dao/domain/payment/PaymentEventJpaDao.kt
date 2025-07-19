d708b027d9b45668048990e6ed7d18aaaec31f64
ilgolf <ilgolc@naver.com>
2025-07-19 18:45:36 +0900

    feat. paymentEvent 상태 변경 API 개발 및 버그 수정

--- infra/src/main/kotlin/me/golf/infra/dao/domain/payment/PaymentEventJpaDao.kt
+++ infra/src/main/kotlin/me/golf/infra/dao/domain/payment/PaymentEventJpaDao.kt
@@ -1,8 +1,15 @@
 package me.golf.infra.dao.domain.payment
 
+import me.golf.core.model.domain.payment.enumerate.EventStatus
 import me.golf.infra.entity.domain.payment.PaymentEventEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface PaymentEventJpaDao: JpaRepository<PaymentEventEntity, Long> {
     fun findByPaymentId(paymentId: Long): PaymentEventEntity?
+
+    @Modifying
+    @Query("update PaymentEventEntity p set p.eventStatus = :eventStatus where p.paymentId = :paymentId")
+    fun updateByPaymentId(paymentId: Long, eventStatus: EventStatus)
 }
